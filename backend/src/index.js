9d592b7e3276da0b666a5d3dfe81fca5d0525357
Gaby Vazquez <gabs.vz@gmail.com>
2019-08-13 21:21:20 -0700

    Cleanup files.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,26 @@
 require('dotenv').config();
 const createServer = require('./createServer');
-const db = require('./db');
 const cors = require('cors');
 
 const server = createServer();
+// TODO: probably I don't need both configurations, but removing any of those makes calls fail. It would need research.
 server.express.use(cors({
-  origin: function(origin, callback){
-    return callback(null, true);
-  },
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:3000"
+  ],
   optionsSuccessStatus: 200,
   credentials: true
 }));
 server
   .start({
-    cors: {
-      credentials: true,
-      origin: [
-        process.env.FRONTEND_URL,
-        "http://localhost:3000"
-      ]
-    }
+      cors: {
+        credentials: true,
+        origin: [
+          process.env.FRONTEND_URL,
+          "http://localhost:3000"
+        ]
+      }
     },
     (details) => console.log(`The server is running on http://localhost:${details.port}`)
   );
--- frontend/src/client/ApolloClient.ts
+++ frontend/src/client/ApolloClient.ts
@@ -2,28 +2,14 @@ import ApolloClient from 'apollo-boost';
 
 export default function createClient() {
   const url = process.env.NODE_ENV === 'development' ? 'http://localhost:4444' : 'https://listera-yoga-prod.herokuapp.com/';
-  console.log(`BACKEND URL: ${url}`);
-  console.log(`NODE ENV:`, process.env, JSON.stringify(process.env));
-
-  const myFetch = (resource: any, init: any) => {
-    const headers = init.headers || {};
-    const newInit = {...init, headers: {...headers, 'Access-Control-Allow-Origin': '*'}};
-    return fetch(resource, newInit);
-  };
 
   return new ApolloClient({
     uri: url,
-    fetch: myFetch,
-    credentials: 'include',
-    headers: { 'Access-Control-Allow-Origin': '*' },
     request: operation => {
       operation.setContext({
         fetchOptions: {
           credentials: 'include',
-          headers: { 'Access-Control-Allow-Origin': '*' }
         },
-        credentials: 'include',
-        headers: { 'Access-Control-Allow-Origin': '*' }
       });
     }
   });
