3f85c5372c49315e38eaa917ee38a79dcee95a65
Gaby Vazquez <gabs.vz@gmail.com>
2019-08-25 16:32:46 -0700

    Configuring CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const createServer = require('./createServer');
 const cors = require('cors');
 
+const whitelist = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000"
+];
 const server = createServer();
 // TODO: probably I don't need both configurations, but removing any of those makes calls fail. It would need research.
 server.express.use(cors({
-  origin: [
-    process.env.FRONTEND_URL,
-    "http://localhost:3000"
-  ],
+  origin: function(origin, callback){
+    if (whitelist.indexOf(origin) !== -1) {
+      return callback(null, true)
+    }
+    return callback(new Error('Not allowed by CORS'))
+
+  },
   optionsSuccessStatus: 200,
   credentials: true
 }));
@@ -16,10 +23,7 @@ server
   .start({
       cors: {
         credentials: true,
-        origin: [
-          process.env.FRONTEND_URL,
-          "http://localhost:3000"
-        ]
+        origin: whitelist
       }
     },
     (details) => console.log(`The server is running on http://localhost:${details.port}`)
