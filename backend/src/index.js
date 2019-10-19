a8f9751585a0eb7885e3f071c7d9c5238a527500
Gaby Vazquez <gabs.vz@gmail.com>
2019-10-18 19:25:28 -0700

    Fixing dev environment.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,18 +4,21 @@ const cors = require('cors');
 
 const whitelist = [
   process.env.FRONTEND_URL,
+  "http://localhost:4444",
   "http://localhost:3000"
 ];
+
+function checkPermissions (origin, callback){
+  if (!origin || whitelist.indexOf(origin) !== -1) {
+    return callback(null, true)
+  }
+  return callback(new Error('Not allowed by CORS'))
+}
+
 const server = createServer();
 // TODO: probably I don't need both configurations, but removing any of those makes calls fail. It would need research.
 server.express.use(cors({
-  origin: function(origin, callback){
-    if (whitelist.indexOf(origin) !== -1) {
-      return callback(null, true)
-    }
-    return callback(new Error('Not allowed by CORS'))
-
-  },
+  origin: checkPermissions,
   optionsSuccessStatus: 200,
   credentials: true
 }));
@@ -23,7 +26,7 @@ server
   .start({
       cors: {
         credentials: true,
-        origin: whitelist
+        origin: checkPermissions
       }
     },
     (details) => console.log(`The server is running on http://localhost:${details.port}`)
